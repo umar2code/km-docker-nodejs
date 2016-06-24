61870c63eea95624de66da23474cad6ef38b79f0
Kishen, Parthu <Parthu.Kishen1@T-Mobile.com>
2016-06-23 19:23:59 -0700

    old files have been deleted and the base features are ready

--- src/app/server.js
+++ src/app/server.js
@@ -7,7 +7,10 @@ var databaseController = require('./controllers/databaseController');
 var keyName;
 var keyValue;
 var connection = null;
-
+/*
+First step is to connect to the rethindb based on the config.js and then create a database called and a table called key if they donot exist. If they exist
+no new db or table are created. A restify server is also started.
+ */
 r.connect(config.rethinkdb, function(err, conn) {
         if (err) {
             console.log('Could not open a connection to initialize the database: ' + err.message);
@@ -28,13 +31,14 @@ r.connect(config.rethinkdb, function(err, conn) {
 server.use(restify.bodyParser());
 server.use(restify.requestLogger());
 
+//this function is for the get operation.
 
 function respond(req, res, next) {
 var requestName=req.params.keyName;
   console.log("Got a request with param: "+requestName );
 
 
-  r.table('key').orderBy(r.desc("timestamp")).pluck(requestName).run(connection, function(err, cursor) {
+  r.db('key').table('key').filter({keyName:requestName}).orderBy(r.desc("timestamp")).pluck('keyValue').run(connection, function(err, cursor) {
     if(err) {
       return next(err);
     }
@@ -43,13 +47,14 @@ var requestName=req.params.keyName;
       if(err) {
         return next(err);
       }
-      console.log("Result:"+JSON.stringify(result[0]).keyName);
-      res.send(result[0].keyName);
+      console.log("Result:"+result[0]);
+      res.send(result[0]);
+      return next();
     });
 
   });
 };
-  
+  //this is for post operation, keyName and keyValue have to be specified in the body. 
 server.post('/key', function create(req, res, next) {
      keyName = req.params.keyName;
      //keyValue = req.params.keyValue;
@@ -57,7 +62,8 @@ server.post('/key', function create(req, res, next) {
      console.log("Value of Key in rethinkdb is   "+keyValue);
 r.table('key').insert([
     { 
-      keyName: keyValue,
+      keyName: keyName,
+      keyValue: keyValue,
       timestamp: new Date()
     }
 ]).run(connection, function(err, result) {
