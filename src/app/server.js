7ff68067147a3662d6c318d475a0d9bd15bdc73c
Kishen, Parthu <Parthu.Kishen1@T-Mobile.com>
2016-07-03 04:09:20 -0700

    added private key generation funcitonality

--- src/app/server.js
+++ src/app/server.js
@@ -71,121 +71,207 @@ function postKey(req, res, next) {
      //keyValue = new Buffer(req.params.keyValue).toString('base64');
      console.log("Value of Key in rethinkdb is   "+keyValue);
   r.table('keys').insert([
-      { 
-        keyName: keyName,
-        keyValue: keyValue,
-        timestamp: new Date()
-      }
-  ]).run(connection, function(err, result) {
-      if (err) throw err;
-      console.log(JSON.stringify(result, null, 2));
-      res.send(201, Math.random().toString(36).substr(3, 8));
-    return next();
-  });  
+          { 
+            keyName: keyName,
+            keyValue: keyValue,
+            timestamp: new Date()
+          }
+      ]).run(connection, function(err, result) {
+          if (err) throw err;
+          console.log(JSON.stringify(result, null, 2));
+          res.send(201, Math.random().toString(36).substr(3, 8));
+        return next();
+      });  
  }
 
 
 
 
 // This API generates passphrase less RSA Keypair based on the input comment(email provided).
-//input email and username
+//input email and username and keyname and it will store the key with that user info:
 
 function generateKeyPair(req, res, next){
 
   var comment = req.params.email;
-  var userName = req.params.username;
+  var keyName = req.params.keyName;
   var pair = keypair();
   var publicKey = forge.pki.publicKeyFromPem(pair.public);
   var publicKeySSH = forge.ssh.publicKeyToOpenSSH(publicKey, comment);
 
-  //console.log(publicKeySSH);
-  res.send(200, {
-    "publicKey": publicKeySSH,
-    "privateKey":pair.private
-  });
-  return next();
 
-
+  r.table('keys').insert([
+          { 
+            username: req.params.username,
+            baseKeyName: keyName,
+            publicKeyName: keyName+'_public',
+            privateKeyName: keyName+'_private',
+            publicKeyValue: publicKeySSH,
+            privateKeyValue: pair.private,
+            timestamp: new Date()
+          }
+      ]).run(connection, function(err, result) {
+          if (err) throw err;
+          console.log(JSON.stringify(result, null, 2));
+          res.send(200, Math.random().toString(36).substr(3, 8));
+        return next();
+      }); 
 }
 
 //This API stores the provided private key in the component. It also stores the information in the DB
-//input parameters are component, componentBaseUrl, ownerUserName, ownerPassword, keyName, publicKeyValue, privateKeyValue
+//input parameters are component, componentBaseUrl, ownerUserName, ownerPassword, keyName
 
-function setupComponentWithKey(req, res, next){
-if(req.params.component == "chefcc"){
+function setupComponentPrivateKey(req, res, next){
+  if(req.params.component == "chefcc"){
 
-console.log("Setup request for ChefCompliance starting...");
-  var jsonObject = JSON.stringify({
-      "userid" : req.params.ownerUserName,
-      "password" : req.params.ownerPassword
+  console.log("Setup request for ChefCompliance starting...");
+    var jsonObject = JSON.stringify({
+        "userid" : req.params.ownerUserName,
+        "password" : req.params.ownerPassword
+    });
+
+    console.log(jsonObject);
+    var postheaders = {
+        'Content-Type' : 'application/json',
+        'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
+    };
+    // the post options
+    var optionspost = {
+        host : req.params.componentBaseUrl,
+        port : 443,
+        path : '/api/login',
+        method : 'POST',
+        headers : postheaders,
+        strictSSL:false,
+        rejectUnhauthorized : false
+    };
+
+    //console.info('Options prepared:');
+    //console.info(optionspost);
+    //console.info('Do the POST call');
+
+
+    // do the POST call
+    var reqPost = https.request(optionspost, function(res1) {
+        console.log("statusCode: ", res1.statusCode);
+        // uncomment it for header details
+    //console.log("headers: ", res.headers);
+
+        res1.on('data', function(data) {
+            //console.info('POST result:\n');
+            //process.stdout.write(data);
+            //console.info('\n\nPOST completed');
+    var AUTH='Bearer '+data;
+    //console.log(AUTH);
+
+
+  r.db('key').table('keys').filter({baseKeyName:req.params.keyName,username:req.params.ownerUserName })
+  .orderBy(r.desc("timestamp"))
+  .pluck('privateKeyValue').run(connection, function(err, cursor) {
+    if(err) {
+      send(500,"Error fetching the key from DB");
+      return next(err);
+    }
+
+    cursor.toArray(function(err, result) {
+      if(err) {
+        send(500,"Error fetching the key from DB");
+        return next(err);
+      }
+      console.log("Result:  \n"+result[0].privateKeyValue);
+
+
+
+      var jsonObjectKey = JSON.stringify({
+                        "name" : req.params.keyName+'_private',
+                        "private" : result[0].privateKeyValue
+                    });
+console.log("here???");
+      var postheadersKey = {
+                'Content-Type' : 'application/json',
+                'Content-Length' : Buffer.byteLength(jsonObjectKey, 'utf8'),
+                'Authorization': AUTH
+            };
+
+
+      var optionspostKey = {
+                host : req.params.componentBaseUrl,
+                port : 443,
+                path : '/api/owners/'+req.params.ownerUserName+'/keys',
+                method : 'POST',
+                headers : postheadersKey,
+                strictSSL:false,
+                rejectUnhauthorized : false
+            };
+
+      
+            var reqPostKey = https.request(optionspostKey, function(res2) {
+                console.log("statusCode: ", res2.statusCode);
+                // uncomment it for header details
+            //console.log("headers: ", res.headers);
+
+                res2.on('data', function(d) {
+                    console.info('POST result from api call to save they key:\n');
+                    process.stdout.write(d);
+                    console.info('\n\nPOST completed');
+                    res.send(200);
+                    return next();
+                });
+            });
+
+            // write the json data
+            reqPostKey.write(jsonObjectKey);
+            reqPostKey.end();
+            reqPostKey.on('error', function(e) {
+              console.error(e);
+              res.send(400);
+              return next();
+                
+            });
+            
+
+      
+    });
+
   });
-  var postheaders = {
-      'Content-Type' : 'application/json',
-      'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
-  };
-  // the post options
-  var optionspost = {
-      host : req.params.componentBaseUrl,
-      port : 443,
-      path : '/api/login',
-      method : 'POST',
-      headers : postheaders,
-      strictSSL:false,
-      rejectUnhauthorized : false
-  };
 
-  //console.info('Options prepared:');
-  //console.info(optionspost);
-  //console.info('Do the POST call');
+            
 
+            
 
-  // do the POST call
-  var reqPost = https.request(optionspost, function(response) {
-      //console.log("statusCode: ", res.statusCode);
-      // uncomment it for header details
-  //  console.log("headers: ", res.headers);
-
-      res.on('data', function(data) {
-          //console.info('POST result:\n');
-          process.stdout.write(data);
-          //console.info('\n\nPOST completed');
-  var AUTH='Bearer '+data;
-  console.log(AUTH);
-
-
-res.send(200, AUTH);
-return next();
+            
 
 
 
-      });
-  });
 
-  // write the json data
-  reqPost.write(jsonObject);
-  reqPost.end();
-  reqPost.on('error', function(e) {
-      console.error(e);
-      res.send(400);
-      return next();
+        });
+    });
 
-  });
+    // write the json data
+    reqPost.write(jsonObject);
+    reqPost.end();
+    reqPost.on('error', function(e) {
+      console.log("in here:errro???");
+        console.error(e);
+        res.send(400);
+        return next();
+
+    });
+
+  }
+  else{
+  console.log("No other component is currently supported");
+  res.send(400);
+  return next();
+
+  }
 
 }
-else{
-console.log("No other component is currently supported");
-res.send(400);
-return next();
-
-}
-
-}
-
 
  server.get('/key/:keyName', getKey);
  server.post('/key', postKey);
  server.post('/keys/keypair',generateKeyPair);
- server.post('/keys/:component/keypairs', setupComponentWithKey);
+ server.post('/keys/:component/keypairs', setupComponentPrivateKey);
+ 
 
  server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
